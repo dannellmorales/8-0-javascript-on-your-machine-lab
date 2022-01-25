ba56786d3fd870a72cb436cfe3a0ca5b9f8deeed
D.Morales <dannellmorales@pursuit.com>
2022-01-25 00:03:21 -0500

    Java & Node

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 /**
- * The function below has no parameters. Instead, access the arguments from the command line.
+ * The function below has no parameters. 
+ * Instead, access the arguments from the command line.
+ * 
  * The first argument passed after the filename should be either "plus" or "minus", which represents the kind of calculation that will be done. Every argument afterwards should be a number.
+ 
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
+
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+ let result = 0 //use number not empty string
+    if (process.argv[2] === "plus"){
+        for(let i = 3; i < process.argv.length; i++){
+            result += process.argv[i]  
+        }
+    }else if(process.argv[2] === "minus"){
+        for(num of process.argv[i]){
+            result += process.argv[i] - (process.argv[1]
+        }
+    }
+ return result
+}
+
+
+
+
+
+
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;
